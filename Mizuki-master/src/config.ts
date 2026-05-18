407f893d9471df3ab58d85648ab008b347a7b7f6
liumo666 <GitHubliumo666@gmail.com>
2026-05-18 22:46:49 +0800

    update:添加了评论区功能

--- Mizuki-master/src/config.ts
+++ Mizuki-master/src/config.ts
@@ -453,10 +453,10 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 };
 
 export const commentConfig: CommentConfig = {
-	enable: false, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
+	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	system: "twikoo", // 评论系统选择: "twikoo" | "giscus"
 	twikoo: {
-		envId: "https://twikoo.vercel.app",
+		envId: "https://dataspace-kappa.vercel.app/",
 		lang: SITE_LANG,
 	},
 	giscus: {
